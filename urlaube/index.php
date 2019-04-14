69933536a7ba96a94a1586f72f6c0d5d40dfb405
Rolfus187 <rolfus187@googlemail.com>
2019-04-14 11:13:21 +0200

    included nav urlaube

--- urlaube/index.php
+++ urlaube/index.php
@@ -22,23 +22,7 @@
     <title>Urlaube</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Urlaube</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="./suedtirol2018/index.php">Südtirol 2018</a>
-        </li>
-		<li class="nav-item">
-          <a class="nav-link" href="./jakobsweg2019/index.php">Jakobsweg 2019</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <?php include ("nav_urlaube.php"); ?>
 
     <div class="container p-3">
       <!-- CSSMap - Continents -->
