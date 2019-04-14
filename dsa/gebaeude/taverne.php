436458ffc6e55c97bf45e7ae711b40050dad8f69
Rolfus187 <rolfus187@googlemail.com>
2019-04-14 11:22:49 +0200

    added full php inclusion of navbar

--- dsa/gebaeude/taverne.php
+++ dsa/gebaeude/taverne.php
@@ -20,34 +20,12 @@
     <title>Taverne</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Kelin Asprat</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-home"></i> Gebäude
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="gebaeude/taverne.html">Taverne</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-people-carry"></i> Angestellte</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-warehouse"></i> Waren</a>
-          </li>
-        </ul>
-          <a href="#" type="button" class="btn btn-secondary">2. Phex 1032 BF</a> &nbsp&nbsp
-          <a href="finanzen.html" type="button" class="btn btn-warning"><i class="fas fa-coins"></i> 500</a>
-      </div>
-    </nav>
+    <?php include ("../nav_dsa.php"); ?>
 
-     <?php include ("dsacard.php"); ?>
+    <?php include ("dsacard.php"); ?>
+    <?php include ("dsacard.php"); ?>
+    <?php include ("dsacard.php"); ?>
+    <?php include ("dsacard.php"); ?>
 
   </body>
 </html>
