ed7b32f350b7074fc822955782dea72668f2b87a
Rolfus187 <rolfus187@googlemail.com>
2019-04-14 11:08:40 +0200

    changed to php, included card

---
+++ dsa/gebaeude/dsacard.php
@@ -0,0 +1,78 @@
+<div class="card m-1 p-0 dsacard">
+  <div class="card-body m-3 p-0">
+    <div class="container m-0 p-0">
+      <div class="row m-0 p-0">
+        <div class="col-12 m-0 p-0">
+          <div class="dsacard-header">Alrik Alrikson</div>
+        </div>
+      </div>
+
+      <div class="row m-0 p-0">
+        <div class="col-6 m-0 p-0">
+          <div class="dsacard-image"></div>
+        </div>
+        <div class="col-6 m-0 p-0">
+          <div class="dsacard-rasse mx-2">Mittelländer</div>
+          <div class="dsacard-infoblock mx-1">Infos</div>
+          <div class="dsacard-potential mx-1">Potential</div>
+        </div>
+      </div>
+
+      <div class="row p-0 my-2 mx-0">
+        <div class="col-6 m-0 p-0 ">
+          <div class="dsacard-talent my-2 mr-1 ml-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Wirtschaft</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 mr-1 ml-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Unterhaltung</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 mr-1 ml-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Kochen</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 mr-1 ml-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Wildnis</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 m-0 p-0">
+          <div class="dsacard-talent my-2 ml-1 mr-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Handwerk</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 ml-1 mr-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Schmied</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">3</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 ml-1 mr-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Baukunst</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+          <div class="dsacard-talent my-2 ml-1 mr-0">
+            <div class="btn-group dsabtn-group">
+              <button type="button" class="btn btn-light btn-block dsabtn-talent-left">Heilkunde</button>
+              <button type="button" class="btn btn-danger dsabtn-talent-right">14</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
