9a4d3c3563989963e9d18855c5f53099655e7d9a
Rolfus187 <rolfus187@googlemail.com>
2019-04-14 11:49:38 +0200

    renamed card css, removed unnessecary stuff

--- dsa/gebaeude/taverne.php
+++ dsa/gebaeude/taverne.php
@@ -15,7 +15,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="custom.css">
+    <link rel="stylesheet" type="text/css" href="dsacard.css">
 
     <title>Taverne</title>
   </head>
